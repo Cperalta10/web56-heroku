9f04530d9064ef25d59f2772ee51433da154cdc9
Christopher Peralta <chrisperalta1998@gmail.com>
2022-08-11 10:34:01 -0700

    commit  before hooking up heroku

--- api/server.js
+++ api/server.js
@@ -2,4 +2,12 @@ const express = require("express");
 
 const server = express();
 
+server.get("/", (req, res) => {
+  res.send("Hello, world!");
+});
+
+server.post("/", (req, res) => {
+  res.json(req.body);
+});
+
 module.exports = server;
